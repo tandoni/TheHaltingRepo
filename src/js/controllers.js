5a5bbc1880dccd8b922cd948d00016bcb45fe02d
Shane S Bruggeman <shanebruggeman@rose-hulman.edu>
2016-02-20 02:14:47 -0500

    started serving sample data

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,4 +1,9 @@
 angular.module('fsm')
     .controller('FsmCtrl', ['$scope', 'FsmDataService', function($scope, FsmDataService) {
         $scope.start = 'start';
+        console.log('started FsmCtrl');
+
+        // FsmDataService.load(function(data) {
+        //     console.log(data);
+        // });
     }]);
--- src/js/services.js
+++ src/js/services.js
@@ -1,4 +1,16 @@
 angular.module('fsm')
     .service('FsmDataService', ['$http', function($http) {
         console.log('loaded FsmDataService');
+
+        this.load = function(callback) {
+            console.log('loading fsm data');
+            $http({
+                    method: 'GET',
+                    url: 'data/aStar.regex'
+                }).then(function (data) {
+                    callback(data.data);
+            }, function (error) {
+                console.log('Failure to retrieve fsm data', error);
+            });
+        };
     }]);
